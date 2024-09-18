3e4a10471361d687036c1af4f80170ed0e6231e0
ShadowDataNerd <94314431+ShadowDataNerd@users.noreply.github.com>
2024-09-18 18:10:32 +0200

    From 4444 to 5555

--- NextGenCrypto_Frontend/src/pages/ToolPage.tsx
+++ NextGenCrypto_Frontend/src/pages/ToolPage.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 
 // API Server URL (lokale Verbindung)
-const API_BASE_URL = "http://127.0.0.1:4444";
+const API_BASE_URL = "http://127.0.0.1:5555";
 
 const ToolPage: React.FC = () => {
     const [url, setUrl] = useState<string>(""); // User-input für die URL
